911399c54a40961b0fcdec172197136f865eddd5
Peiqi <56414020+shmilypt@users.noreply.github.com>
2019-10-10 20:27:46 -0400

    Add files via upload

---
+++ cs111-a2/OrderCheck.java
@@ -0,0 +1,30 @@
+/*************************************************************************
+ *  Compilation:  javac OrderCheck.java
+ *  Execution:    java OrderCheck 5 10 15 2
+ *
+ *  @author: Peiqi Tang
+ *
+ *  Prints true if the four integer inputs are in strictly ascending
+ *  or descending order, false otherwise
+ *
+ *  % java OrderCheck 5 10 15 2
+ *  false
+ *
+ *  % java OrderCheck 15 11 9 4
+ *  true
+ *
+ *************************************************************************/
+
+public class OrderCheck {
+
+    public static void main(String[] args) {
+
+	// WRITE YOUR CODE HERE
+    int w = Integer.parseInt(args[0]);
+    int x = Integer.parseInt(args[1]);
+    int y = Integer.parseInt(args[2]);
+    int z = Integer.parseInt(args[3]);
+
+   	System.out.println(w<x && x<y && y<z || w>x && x>y && y>z );
+    }
+}
---
+++ cs111-a2/WindChill.java
@@ -0,0 +1,35 @@
+/*************************************************************************
+ *  Compilation:  javac WindChill.java
+ *  Execution:    java WindChill 35.0 10.0
+ *
+ *  @author: Peiqi Tang
+ *
+ *  That takes two double command-line arguments temperature and velocity 
+ *  and prints the wind chill (a double) according to the following:
+ *
+ *  w = 35.74 + 0.6215 * T + (0.4275 * T - 35.75) v^0.16
+ *
+ *  % java WindChill 35.0 10.0
+ *  27.445420765619037
+ *
+ *  The formula is not valid if T is larger than 50 in absolute value or if 
+ *  v is larger than 120 or less than 3.
+ *
+ *  Assume the inputs are valid values
+ *
+ *
+ *************************************************************************/
+
+public class WindChill {
+
+    public static void main(String[] args) {
+
+	// WRITE YOUR CODE HERE
+	Double temperatureC = Double.parseDouble(args[0]);
+	Double speedV = Double.parseDouble(args[1]);
+
+	double w = 35.74 + 0.6215*temperatureC + (0.4275*temperatureC - 35.75)* (Math.pow(speedV,0.16));
+	System.out.println(w);
+
+    }
+}
