57f45eac016648f4fe9ee9bbd5ea0ea2b1803b15
Peiqi <56414020+shmilypt@users.noreply.github.com>
2019-11-15 22:31:20 -0500

    Add files via upload

--- cs111-a6/RunLengthEncoding.java
+++ cs111-a6/RunLengthEncoding.java
@@ -6,8 +6,7 @@
  *
  *************************************************************************/
 
-public class RunLengthEncoding
-{
+public class RunLengthEncoding{
 
     /*
      * Encode the original string by finding sequences in the string
@@ -16,22 +15,21 @@ public class RunLengthEncoding
      * followed by the repeating character.
      * Return the encoded string.
      */
-    public static String encode (String original)
-    {
+    public static String encode (String original){
 
         // WRITE YOUR CODE HERE
         String encodeString = new String();
         int n = original.length();
-        for (int i = 0; i < n; i++)
-        {
+        for (int i = 0; i < n; i++){
             int count = 1;
-            while (i < n - 1 && original.charAt(i) == original.charAt(i + 1))
-            {
+            while (i < n - 1 && original.charAt(i) == original.charAt(i + 1)){
                 count++;
                 i++;
             }
-            if (count != 1) System.out.print(count);
-            System.out.print(original.charAt(i));
+            if (count != 1) {
+            	encodeString = encodeString + count;
+            }
+            encodeString = encodeString + original.charAt(i);
         }
         return encodeString;
 
@@ -44,64 +42,37 @@ public class RunLengthEncoding
      * YOUR decode METHOD MUST BE RECURSIVE, do not use while, do/while,
      * or for loops
      */
-    public static String decode (String original)
-    {
+    public static String decode (String original){
 
         // WRITE YOUR RECURSIVE CODE HERE
-        /*String encodeString = new String();
+        int length = original.length();
+        if (length == 0){
+			return "";
+		}
         String decodeString = new String();
+		char a = original.charAt(0);
+		if (Character.isDigit(a)){
+			int index = a-'0';
+			if (index == 0){
+				return decode(original.substring(2));
+			}else{
+				int preIndex = index-1; 
+				return original.charAt(1)+decode(preIndex+original.substring(1));	
+			}
+		}else{
+			return a + decode(original.substring(1));
+			}
+		}
 
 
-
-
-        return decodeString;
-        */
-
-        String decodedString = new String();
-        int n = original.length() - 1;
-        if (n == 0)
-        {
-            if (Character.isDigit(original.charAt(n)) == false)
-            {
-                decodedString = original.charAt(n) + decodedString;
-            }
-            return decodedString;
-        }
-        else
-        {
-
-            if (Character.isDigit(original.charAt(n)) == true)
-            {
-                original = original.substring(0, n);
-            }
-            else
-            {
-                if (Character.isDigit(original.charAt(n - 1)) == false || original.charAt(n - 1) == 1)
-                {
-                    decodedString = original.charAt(n) + decodedString;
-                    original = original.substring(0, n);
-                }
-                else
-                {
-                    decodedString = original.charAt(n) + decodedString;
-                    char a = original.charAt(n - 1);
-                    a--;
-                    original = original.substring(0, n - 1) + a + original.charAt(n);
-                }
-            }
-            return decode(original) + decodedString;
-        }
-
-    }
-
-    public static void main (String[] args)
-    {
+    public static void main (String[] args){
 
         // WRITE YOUR TEST CASES HERE to test your methods
 
         String str = "qwwwwwwwwweeeeerrtyyyyyqqqqwEErTTT";
         String rts = "q9w5e2rt5y4qw2Er3T";
         System.out.println(encode(str));
-        System.out.println(decode(rts));
+        System.out.print(decode(rts));
+
     }
 }
